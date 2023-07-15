e4f872e07562b7bbdaf07f36f70152c21cae874f
sunil kumar <40545242+sunilkumarasamy@users.noreply.github.com>
2023-07-15 08:23:21 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,23 @@
 import pandas as pd
 for i in range(0,10):
     print(i)
+
+
+
+import pandas as pd
+import numpy as np
+
+# Example DataFrame
+df = pd.DataFrame({
+    'A': [1, -2, 3, -4],
+    'B': [5, 6, -7, 8],
+    'C': [-9, 10, -11, 12]
+})
+
+# Iterate over each row and change values less than 0 to null
+for index, row in df.iterrows():
+    for column in df.columns:
+        if row[column] < 0:
+            df.at[index, column] = np.nan
+
+print(df)
